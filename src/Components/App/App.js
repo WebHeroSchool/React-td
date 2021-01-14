e8d5aad2ad36fc6bc15e17b293701905c4300d2a
Anna <annamt1773@gmail.com>
2021-01-14 16:41:38 +0300

    урок 21 список эл-тов и ключи

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,13 +3,26 @@ import ItemList from '../ItemList/ItemList';
 import InputItems from '../InputItems/InputItems';
 import Footer from '../Footer/Footer';
 
-const todoItem = 'Написать новое приложение';
+const App = () => {
+     const items = [
+         {
+            value: 'Написать новое приложение'
+         },
+         {
+            value: 'props'
+         },
+         {
+           value: 'сделать урок'
+         }
+     ];
 
-const App = () => (<div>
-  <h1> Список дел: </h1>
-  <InputItems />
-  <ItemList todoItem={todoItem} />
-  <Footer count={3} />
-</div>);
+     return (
+    <div>
+        <h1> Список дел: </h1>
+        <InputItems />
+        <ItemList items={items} />
+        <Footer count={3} />
+    </div>);
+}
 
 export default App;
