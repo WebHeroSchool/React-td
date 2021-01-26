34d6252cd8d91733306cf2457d31b443a9956bdb
Anna <annamt1773@gmail.com>
2021-01-26 13:24:13 +0300

    урок 30 Изменение состояния

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -12,21 +12,36 @@ class App extends React.Component {
       items: [
           {
              value: 'Написать новое приложение',
-             isDone: true
+             isDone: true,
+             id: 1
           },
           {
              value: 'props',
-             isDone: true
+             isDone: true,
+             id: 2
           },
           {
             value: 'сделать урок',
-            isDone: false
+            isDone: false,
+            id: 3
           }
-      ]
+      ],
+      count: 3
     };
 
-  onClickDone = isDone => console.log(isDone);
-    
+  onClickDone = id => {
+      const newItemList = this.state.items.map(item => {
+          const newItem = {...item };
+
+          if (item.id === id) {
+              newItem.isDone = !item.isDone;
+          }
+
+          return newItem;
+      });
+      this.setState({ items: newItemList });
+  };
+
   render () {
       return (
           <div className={styles.wrap}>
@@ -35,7 +50,7 @@ class App extends React.Component {
                       <h1> Список дел: </h1>
                       <InputItems />
                       <ItemList items={this.state.items} onClickDone={this.onClickDone}/>
-                      <Footer count={3} />
+                      <Footer count={this.state.count} />
                   </CardContent>
               </Card>
           </div>);
