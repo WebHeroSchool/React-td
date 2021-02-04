7b5262483e429413b5144e06e2ef027d8a6ae929
Anna <annamt1773@gmail.com>
2021-02-04 19:10:22 +0300

    урок 36 утечка памяти

--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -16,15 +16,11 @@ const styles = {
 
 class Item extends React.Component {
   componentDidMount () {
-    console.log('componentDidMount');
-  }
-
-  componentDidUpdate () {
-    console.log('componentDidUpdate');
+    this.timerID = setInterval(() => console.log('interval'), 1000);
   }
 
   componentWillUnmount() {
-    console.log('componentWillUnmount');
+    clearInterval(this.timerID);
   }
 
   render () {
